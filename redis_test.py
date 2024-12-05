d764672783649082955230d0bd27c64acbd32b58
Zainab Bharucha <zainabbharucha11@gmail.com>
2024-12-05 01:36:14 -0800

    Updated redis test script

--- redis_test.py
+++ redis_test.py
@@ -1,11 +1,12 @@
 import time
 import redis
+import random
 
 # Connect to Redis
 client = redis.Redis(host='localhost', port=6381, decode_responses=True)
 
 # Benchmark TRIE.ADD
-start = time.time()
+# start = time.time()
 # for i in range(10000):  # Adjust the range for larger datasets
 #     client.execute_command("TRIE.ADD", f"key{i}", f"value{i}")
 # end = time.time()
@@ -17,23 +18,50 @@ start = time.time()
 #     client.execute_command("TRIE.SEARCH", f"key{i}")
 # end = time.time()
 # print("TRIE.SEARCH latency for 10,000 items:", end - start)
-TRIE.ADD latency for 10,000 items: 0.2531242370605469
-TRIE.SEARCH latency for 10,000 items: 0.22985076904296875
 
-HSET latency for 10,000 items: 0.2665598392486572
-HGET latency for 10,000 items: 0.24653959274291992
+list = []
+with open("words_alpha.txt") as fp:
+    while (True):
+        line = fp.readline()
+        if not line:
+            break
+        list.append(line)
 
-# Benchmark HSET
-start = time.time()
-for i in range(10000):
-    client.hset("hash", f"key{i}", f"value{i}")
-end = time.time()
-print("HSET latency for 10,000 items:", end - start)
+print("Test Case Length | HSET_ADD Time         | HSET_SEARCH Time      | TRIE_ADD Time         | TRIE_SEARCH Time")
+print(("-" * 17) + "+" + ("-" * 23) + "+" + ("-" * 23) + "+" + ("-" * 23) + "+" + ("-" * 23))
 
-# Benchmark HGET
-start = time.time()
-for i in range(10000):
-    client.hget("hash", f"key{i}")
-end = time.time()
-print("HGET latency for 10,000 items:", end - start)
+test_case_length_list = [10, 100, 1000, 10000]
+for test_case_length in test_case_length_list:
+    test_list = random.sample(list, k = 2 * test_case_length)
+    key_list = test_list[:test_case_length]
+    value_list = test_list[test_case_length:]
 
+    start = time.time()
+    for i in range(test_case_length):
+        client.hset("hash", f"{key_list[i]}", f"{value_list[i]}")
+    end = time.time()
+    hset_add_time = end - start
+    # print(f"HSET latency for {test_case_length} items: {hset_add_time}")
+
+    start = time.time()
+    for i in range(test_case_length):
+        client.hget("hash", f"{key_list[i]}")
+    end = time.time()
+    hset_search_time = end - start
+    # print(f"HSET latency for {test_case_length} items: {hset_search_time}")
+
+    start = time.time()
+    for i in range(test_case_length):
+        client.execute_command("TRIE.ADD", f"{key_list[i]}", f"{value_list[i]}")
+    end = time.time()
+    trie_add_time = end - start
+    # print(f"TRIE latency for {test_case_length} items: {trie_add_time}")
+
+    start = time.time()
+    for i in range(test_case_length):
+        client.execute_command("TRIE.SEARCH", f"{key_list[i]}")
+    end = time.time()
+    trie_search_time = end - start
+    # print(f"TRIE latency for {test_case_length} items: {trie_search_time}\n")
+
+    print(f"{test_case_length}" + (" " * (16 - len(str(test_case_length)))) + " | " + f"{hset_add_time}" + (" " * (21 - len(str(hset_add_time)))) + " | " + f"{hset_search_time}" + (" " * (21 - len(str(hset_search_time)))) + " | " + f"{trie_add_time}" + (" " * (21 - len(str(trie_add_time)))) + " | " + f"{trie_search_time}" + (" " * (21 - len(str(trie_search_time)))))
